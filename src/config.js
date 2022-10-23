f42d99424f524684f33329f648c043c1b70f859e
Arda Örkin <ardaorkin@gmail.com>
2022-10-23 14:23:48 +0300

    aws api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://mighty-meadow-28489.herokuapp.com/api/v1";
-export const API_URL = "https://54.210.89.111:3001/api/v1";
+export const API_URL = "http://ec2-3-86-164-220.compute-1.amazonaws.com/api/v1";
