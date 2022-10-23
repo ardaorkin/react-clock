596f930d99d1bf1055fe48b67356ddb41f232830
Arda Örkin <ardaorkin@gmail.com>
2022-10-23 11:31:42 +0300

    add api on aws

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://mighty-meadow-28489.herokuapp.com/api/v1";
-export const API_URL = "http://localhost:3001/api/v1";
+export const API_URL = "https://54.210.89.111:3001/api/v1";
