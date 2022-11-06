c97dabcdd13f967f2c5c0139125b48382617a7f2
Arda Örkin <ardaorkin@gmail.com>
2022-11-06 19:22:15 +0300

    use external api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3001/api/v1";
-// export const API_URL = "https://3.86.164.220.nip.io/api/v1";
+// export const API_URL = "http://localhost:3001/api/v1";
+export const API_URL = "https://3.86.164.220.nip.io/api/v1";
